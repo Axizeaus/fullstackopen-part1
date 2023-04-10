22f99001804d53fa446479e254f24491024306df
Axizeaus <axizeaus@gmail.com>
2023-04-10 10:11:11 +0630

    unicafe finished

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -6,28 +6,56 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const all = good + neutral + bad;
-  const postive = (good / all) * 100;
-  const average = (good - bad) / all
-
   return (
     <>
       <h1>Give Feedback</h1>
       <Button handleClick={() => setGood(good + 1)} name="good" />
-      <br />
       <Button handleClick={() => setNeutral(neutral + 1)} name="neutral" />
-      <br />
       <Button handleClick={() => setBad(bad + 1)} name="bad" />
-      <br />
-      <div>good : {good}</div>
-      <div>neutral : {neutral}</div>
-      <div>bad : {bad}</div>
-      <div>positive : {postive}%</div>
-      <div>all : {all}</div>
-      <div>average : {average}</div>
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   );
 };
+
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad;
+  const positive = (((good / all) * 100) || 0).toFixed(2);
+  const average = (((good - bad) / all) || 0).toFixed(2);
+
+  if (all === 0){
+    return <h2>No Feedback Given</h2>
+  } else {
+    return (
+      <>
+        <table>
+          <tbody>
+            <StatisticsLine text="good" value={good} />
+            <StatisticsLine text="neutral" value={neutral} />
+            <StatisticsLine text="bad" value={bad} />
+            <StatisticsLine text="all" value={all} />
+            <StatisticsLine text="average" value={average} />
+            <StatisticsLine text="positive" value={positive} />
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
+
+const StatisticsLine = ({text, value}) => {
+  if (text === 'positive') {
+    value = String(value) + '%'
+  }
+  return (
+    <>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+    </>
+  );
+}
 
 const Button = ({handleClick, name}) => {
   return (
